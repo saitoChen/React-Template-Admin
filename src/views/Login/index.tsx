ea8c4ee8e3381ffd046b344977d9f2487dde4444
chenjianfeng <chenjianfeng93@163.com>
2023-01-06 00:26:16 +0800

    feat: update login.tsx style

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,10 +1,49 @@
 
 import type { FC } from 'react'
+import { Divider } from 'antd'
+import { Input, Form, Button } from 'antd';
+import './login.scss'
 
 const Login:FC = () => {
 
     return (
-        <div>登录页</div>
+        <>
+            <section className="login-wrapper flex">
+                <section className="left-section">
+                    <h1 className="font48 bold">天工</h1>
+                    <h5>react-admin-template</h5>
+                </section>
+                <Divider className="login-diveder" type="vertical" />
+                <section className="right-section">
+                <Form
+                    name="login"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    autoComplete="off"
+                    className='family'
+                    >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input placeholder='Jeff' />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password placeholder='123456' />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button >Login</Button>
+                    </Form.Item>
+                </Form>
+                </section>
+            </section>
+        </>
     )
 }
 
