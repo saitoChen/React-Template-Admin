c09b75000a52c66a2d98bbdcff1a9197f46a430e
chenjianfeng <chenjianfeng93@163.com>
2023-01-14 22:48:17 +0800

    feat: update login page's style

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,48 +1,74 @@
 
 import type { FC } from 'react'
+import { useState } from 'react'
 import { Divider } from 'antd'
-import { Input, Form, Button } from 'antd';
+import { Input, Form, Button, Select } from 'antd';
 import './login.scss'
 
-const Login:FC = () => {
+const { Option } = Select
 
+type username = 'Jeff' | 'May' | 'Tom'
+interface LoginUser  {
+    username: username
+    password: number
+}
+
+
+const Login:FC = () => {
+    const onFinish = (value: any) => {
+        console.log(value)
+    }
     return (
         <>
-            <section className="login-wrapper flex">
-                <section className="left-section">
-                    <h1 className="font48 bold">天工</h1>
-                    <h5>react-admin-template</h5>
-                </section>
-                <Divider className="login-diveder" type="vertical" />
-                <section className="right-section">
-                <Form
-                    name="login"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    autoComplete="off"
-                    className='family'
-                    >
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
-                    >
-                        <Input placeholder='Jeff' />
-                    </Form.Item>
+            <div className="login-wrapper">
+                <section className="login-content flex">
+                    <section className="left-section">
+                        <h1 className="font48 bold">天工</h1>
+                        <h5>react-admin-template</h5>
+                    </section>
+                    <Divider className="login-diveder" type="vertical" />
+                    <section className="right-section">
+                    <Form
+                        name="login"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        initialValues={{ username: 'jeff' }}
+                        autoComplete="off"
+                        className='family'
+                        onFinish={onFinish}
+                        >
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input placeholder='Jeff' />
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                        <Input.Password placeholder='123456' />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button >Login</Button>
-                    </Form.Item>
-                </Form>
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password placeholder='123456' />
+                        </Form.Item>
+                        <Form.Item  className="login-btn" wrapperCol={{ offset: 6, span: 2 }}>
+                            <Button htmlType="submit">Login</Button>
+                        </Form.Item>
+                        <Form.Item  className="login-btn quick-login"  name="changeUser" wrapperCol={{ offset: 20, span: 16 }}>
+                            <Select
+                            placeholder="quick login"
+                            allowClear
+                            >
+                            <Option value="male">administer</Option>
+                            <Option value="female">manager</Option>
+                            <Option value="other">saler</Option>
+                            </Select>
+                        </Form.Item>
+                    </Form>
+                    </section>
                 </section>
-            </section>
+            </div>
         </>
     )
 }
